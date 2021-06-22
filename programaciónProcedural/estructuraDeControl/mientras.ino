6b76eb8e06aaf3ec6e374c37c2c2cda7d41b7fd7
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-06-22 03:53:30 -0300

    Create mientras.ino

---
+++ programaciónProcedural/estructuraDeControl/mientras.ino
@@ -0,0 +1,16 @@
+//"MIENTRAS" COMUN
+  while( /* condicion */ ){
+    /* codigo que se ejecuta mientra que la condicion sea verdadera */
+  }
+  /*  LA PRIMERA VEZ SE CONSULTA LA CONDICION, SI ES VERDADERA SE EJECUTA EL
+      CODIGO ENTRE LLAVES, AL FINALIZAR ESA EJECUCION SE VUELVE A CONSULTAR
+      LA CONDICION Y SI ES VERDADERA SE VUELVE A REPETIR EL CODIGO Y ASÍ
+      CICLICAMENTE HASTA QUE LA CONDICION SEA FALSA.
+  */
+
+
+//"MIENTRAS" CON SU CONDICION AL FINAL
+  do {
+    /* codigo que se ejecuta al menos una vez y se seguirá ejecutando mientras
+    que la condicion sea verdadera */
+  } while( /* condicion */ );
