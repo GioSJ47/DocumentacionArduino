de6a208e66e475fb813184d767e09b0da7e24580
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-06-22 03:41:49 -0300

    Create siLogico.ino

---
+++ programaciónProcedural/estructuraDeControl/siLogico.ino
@@ -0,0 +1,24 @@
+//"SI" LOGICO SIMPLE
+  if( /* condicion */ ){
+    /* codigo a ejecutar si la condicion es verdadera */
+  }
+
+
+//"SI" Y "SI NO" LOGICO
+  if( /* condicion */ ){
+    /* codigo a ejecutar si la condicion es verdadera */
+  } else {
+    /* codigo a ejecutar si la condicion es FALSA */
+  }
+
+
+//"SI", "SI, SI NO" Y "SI NO" LOGICOS
+  if( /* condicion */ ){
+    /* codigo a ejecutar si la condicion es verdadera */
+  } else if( /* segunda posible condicion */ ){
+    /* codigo a ejecutar si la condicion logica anterior fue falsa y esta otra condicion si es verdadera */
+  } else if( /* tercera posible condicion */ ){
+    /* codigo a ejecutar si la condicion anterior fue falsa y esta otra condicion si es verdadera */
+  } else {
+    /* codigo a ejecutar si todas las condiciones anteriores terminaron siendo falsas */
+  }
