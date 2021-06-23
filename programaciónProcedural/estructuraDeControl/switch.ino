3baf4af2c1fb2b65598132ca2e2e37cc23a456d8
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-06-23 17:24:25 -0300

    Update switch.ino

--- programaciónProcedural/estructuraDeControl/switch.ino
+++ programaciónProcedural/estructuraDeControl/switch.ino
@@ -32,3 +32,63 @@
 
 
 //"SWITCH" CON UN CASO DEFAULT
+  switch ( /* variable que se usara para decidir que caso sucederá */ ){
+    case /* caso 1 */ :
+      /* codigo */
+      break;
+      
+    case /* caso 2 */ :
+      /* codigo */
+      break;
+      
+    default /* caso alternativo */ :
+      /* codigo */
+      //NO ES NECESARIO HACER USO DE LA INSTRUCCION break
+  }
+
+
+//"SWITCH" CON MAS DE UNA POSIBILIDAD POR CASO
+  int x = 122;
+
+  switch ( x ){
+    case ( 20, 25 ):
+      /* codigo */
+      break;
+      
+    case ( 30 ):
+      /* codigo */
+      break;
+    
+    case ( 80, 100, 122, 200 ):
+      /* codigo */
+      break;
+      
+    default /* caso alternativo */ :
+      /* codigo alternativo */
+  }
+  //SE EJECUTARÁ EL TERCER CASO PORQUE x ALMACENA 122
+
+
+//"SWITCH" HACIENDO USO DE MAS DE UNA VARIABLE ( COMPORTAMIENTO DE "O LOGICO" ( || ) )
+  int x = 10;
+  char c = 'b';
+
+  switch ( x, c ){
+    case ( 20, 'a', 'b' ):
+      /* codigo */
+      break;
+      
+    case ( 25, 'c' ):
+      /* codigo */
+      break;
+    
+    case ( 100 ):
+      /* codigo */
+      break;
+      
+    default /* caso alternativo */ :
+      /* codigo alternativo */
+  }
+  //EN ESTE CASO SE EJECUTARÁ EL PRIMER CASO PORQUE LA VARIABLE c ALMACENA 'b'
+  //FLUJO DE EJECUCION:
+  //    x!=20 x!='a' x!='b' c!=20 c!='a' c=='b' ~se ejecuta primer caso~
