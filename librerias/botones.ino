e2b1b14b9a50f224ee4efb3279ddca703aeff277
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2022-05-21 08:28:56 -0300

    Update botones.ino

--- librerias/botones.ino
+++ librerias/botones.ino
@@ -1,11 +1,21 @@
-/* BOTONES */
+/* INFORMACION * /
+  DEFINICION:
+    Esta librería no solo es útil para el manejo de botones, es útil para cualquier componente digitál.
+  
+  FUNCIONES:
+    buttonRead(BotonID):
+      La función devuelve 0 si el botón no está siendo precionado.
+      La función devuelve 1 si el botón está siendo precionado.
+      La función devuelve 2 si el botón ya lleva rato siendo precionado.
+
+/* RECURSO */
   //PINES PARA BOTONES
     byte BTN[] = { 2, 4, 7 };
 
   //NO TOCAR
     bool BTN_BOOL[sizeof(BTN)];
     unsigned long BTN_T[sizeof(BTN)];
-    byte BTN_MILLIS = 5;
+    byte BTN_MILLIS = 5; //ANTI FALSO CONTACTO MECANICO
 
   //FUNCION
     byte buttonRead (byte id = 0) {
@@ -30,16 +40,17 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
-  Serial.print("A: ");
-  Serial.println((int)buttonRead(0));
-  
-  Serial.print("B: ");
-  Serial.println((int)buttonRead(1));
-  
-  Serial.print("C: ");
-  Serial.println((int)buttonRead(2));
-  
-  Serial.println("");
-  delay(1000);
-}
+/* EJEMPLO DE USO */
+  void loop() {
+    Serial.print("A: ");
+    Serial.println((int)buttonRead(0));
+
+    Serial.print("B: ");
+    Serial.println((int)buttonRead(1));
+
+    Serial.print("C: ");
+    Serial.println((int)buttonRead(2));
+
+    Serial.println("");
+    delay(1000);
+  }
